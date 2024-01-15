cf844af9ba2520673672031793d62b9fcaaeabdf
shahana <rafiyashahana@gmail.com>
2024-01-15 11:58:32 +0300

    password & confirm pass validated

--- sign-up.js
+++ sign-up.js
@@ -1,7 +1,7 @@
 const username = document.querySelector("#name-signup");
 const email = document.querySelector("#email-signup");
-const password = document.querySelector("#password-signup");
-const cpassword = document.querySelector("#cpassword-signup");
+const pass = document.querySelector("#password-signup");
+const cpass = document.querySelector("#cpassword-signup");
 const signUpBtn = document.querySelector("#sign-up-btn");
 const errorMsg = document.querySelectorAll(".error-msg");
 const input = document.querySelectorAll("input");
@@ -14,11 +14,13 @@ signUpBtn.addEventListener("click", (e) => {
 const validateForm = () => {
   validateUser();
   validateEmail();
+  validatePass();
+  //   confirmPass();
 };
 
 const validateUser = () => {
   if (username.value === "") {
-    showError(0, "Username can't be empty");
+    showError(0, "Username can't be empty. Please fill it in.");
   } else if (username.value.length < 3 || username.value.length > 15) {
     showError(0, "Username must be between 3 and 15 characters");
   } else {
@@ -30,7 +32,7 @@ const validateEmail = () => {
   const re =
     /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
   if (email.value == "") {
-    showError(1, "Email can't be empty");
+    showError(1, "Email can't be empty. Please fill it in.");
   } else if (!email.value.match(re)) {
     showError(1, "Enter a valid email");
   } else {
@@ -38,6 +40,27 @@ const validateEmail = () => {
   }
 };
 
+const validatePass = () => {
+  if (pass.value == "" || cpass.value == "") {
+    showError(2, "Password can't be empty. Please fill it in.");
+    showError(3, "Confirm password can't be empty. Please fill it in.");
+  } else if (pass.value.length < 8 || pass.value.length > 15) {
+    showError(2, "Password must be between 8 and 15 characters");
+    showError(3, "");
+  } else {
+    showSuccess(2);
+    confirmPass();
+  }
+};
+
+const confirmPass = () => {
+  if (cpass.value !== pass.value) {
+    showError(3, "Passwords do not match");
+  } else {
+    showSuccess(3);
+    showSuccess(2);
+  }
+};
 const showError = (i, msg) => {
   errorMsg[i].style.display = "block";
   errorMsg[i].innerHTML = msg;
